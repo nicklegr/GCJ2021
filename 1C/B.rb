8e0be7c596de7d8c78d518c006770e8399489e34
nicklegr <1327502+nicklegr@users.noreply.github.com>
2021-05-01 20:11:11 +0900

    1C: B-small

---
+++ 1C/B.rb
@@ -0,0 +1,141 @@
+require 'pp'
+
+def ppd(*arg)
+  if $DEBUG
+    arg.each do |e|
+      PP.pp(e, STDERR)
+    end
+  end
+end
+
+def putsd(*arg)
+  if $DEBUG
+    STDERR.puts(*arg)
+  end
+end
+
+def parrd(arr)
+  putsd arr.join(" ")
+end
+
+def ri
+  readline.to_i
+end
+
+def ris
+  readline.split.map do |e| e.to_i end
+end
+
+def rs
+  readline.chomp
+end
+
+def rss
+  readline.chomp.split
+end
+
+def rf
+  readline.to_f
+end
+
+def rfs
+  readline.split.map do |e| e.to_f end
+end
+
+def rws(count)
+  words = []
+  count.times do
+    words << readline.chomp
+  end
+  words
+end
+
+def puts_sync(str)
+  puts str
+  STDOUT.flush
+end
+
+def array_2d(r, c)
+  ret = []
+  r.times do
+    ret << [0] * c
+  end
+  ret
+end
+
+class Integer
+  def popcount32
+    bits = self
+    bits = (bits & 0x55555555) + (bits >>  1 & 0x55555555)
+    bits = (bits & 0x33333333) + (bits >>  2 & 0x33333333)
+    bits = (bits & 0x0f0f0f0f) + (bits >>  4 & 0x0f0f0f0f)
+    bits = (bits & 0x00ff00ff) + (bits >>  8 & 0x00ff00ff)
+    return (bits & 0x0000ffff) + (bits >> 16 & 0x0000ffff)
+  end
+
+  def combination(k)
+    self.factorial/(k.factorial*(self-k).factorial)
+  end
+
+  def permutation(k)
+    self.factorial/(self-k).factorial
+  end
+
+  def factorial
+    return 1 if self == 0
+    (1..self).inject(:*)
+  end
+end
+
+def check(c)
+  ml = c.to_s.size/2
+  for l in 1 .. ml
+    cs = c.to_s
+    f = cs[0, l].to_i
+    cs = cs[l, 100]
+    ok = true
+    while !cs.empty?
+      f += 1
+      if cs.start_with?(f.to_s)
+        cs = cs[f.to_s.size, 100]
+      else
+        ok = false
+        break
+      end
+    end
+
+    if ok
+      return c
+    end
+  end
+
+  nil
+end
+
+# main
+t_start = Time.now
+
+cases = readline().to_i
+
+(1 .. cases).each do |case_index|
+  y = ri
+
+  for c in y+1..10**18
+    if check(c)
+      puts "Case ##{case_index}: #{c}"
+      break
+    end
+  end
+
+  # progress
+  trigger = 
+    if cases >= 10
+      case_index % (cases / 10) == 0
+    else
+      true
+    end
+
+  if trigger
+    STDERR.puts("case #{case_index} / #{cases}, time: #{Time.now - t_start} s")
+  end
+end
